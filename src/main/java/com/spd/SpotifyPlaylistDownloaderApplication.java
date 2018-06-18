a90541fd70c0c3cba32f6bb8d6e566df2d6510a8
ivanmilevtues <ivanmilevtues@gmail.com>
2018-06-18 19:05:41 +0300

    Created Spring Project

---
+++ src/main/java/com/spd/SpotifyPlaylistDownloaderApplication.java
@@ -0,0 +1,12 @@
+package com.spd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyPlaylistDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyPlaylistDownloaderApplication.class, args);
+	}
+}
